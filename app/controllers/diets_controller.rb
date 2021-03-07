4215172e2f0ba700d7107cffd8fbacb745ce098e
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-03-07 12:47:40 -0300

    tradução de layout pt-br

--- app/controllers/diets_controller.rb
+++ app/controllers/diets_controller.rb
@@ -1,6 +1,6 @@
 class DietsController < ApplicationController
   before_action :set_diet, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!
   # GET /diets or /diets.json
   def index
     @diets = Diet.all
